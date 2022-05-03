64e7bc768e5286f74a76e75d8133062544788f78
macie <macie@gmail.com>
2022-05-03 15:52:10 -0500

    siuu

---
+++ firebase.js
@@ -0,0 +1,23 @@
+/*
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCFAqSpCmqWV4lziZUekNhQel7j90Qio8w",
+    authDomain: "tienda-macheo.firebaseapp.com",
+    projectId: "tienda-macheo",
+    storageBucket: "tienda-macheo.appspot.com",
+    messagingSenderId: "699545661217",
+    appId: "1:699545661217:web:50d81fb3b8c2c119595860",
+    measurementId: "G-PWN65K50VF"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+*/
