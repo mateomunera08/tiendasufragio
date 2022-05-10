437061fb442b617aafeda3fff0d3affc92f610a6
yfddf <you@gmail.com>
2022-05-10 17:22:23 -0500

    sioo

--- controlador.js
+++ controlador.js
@@ -1,107 +1,142 @@
 //OBJETOS
 let producto1={
-    nombre:"producto1",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"agua con gas",
+    precio:2000,
+    estado:"true",
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-macheo.appspot.com/o/agua%20con%20gas.png?alt=media&token=38d135e4-d98c-4c5d-ba46-bf97fb6c5bf1"
 }
 
 let producto2={
-    nombre:"producto2",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"agua sin gas",
+    precio:2200,
+    estado:"true",
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-macheo.appspot.com/o/agua%20sin%20ags.png?alt=media&token=19af20a9-4101-4a5c-a253-5af3b4233cbf"
 }
 
 let producto3={
-    nombre:"producto3",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"azucar",
+    precio:3000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-macheo.appspot.com/o/azucar.png?alt=media&token=87af9d49-b4a4-4156-8909-01b988ff1399"
 }
 
 let producto4={
-    nombre:"producto4",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"bascula",
+    precio:200000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-macheo.appspot.com/o/bascula.jpg?alt=media&token=3ed61b89-ebad-4dea-b81e-934d9584e20c"
 }
 
 let producto5={
-    nombre:"producto5",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"bateria portatil",
+    precio:100000,
+    estado:false,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-macheo.appspot.com/o/bateria%20portatil.jpg?alt=media&token=c768706b-0eec-4c64-8a3e-3a9c2cd2aa0e"
 }
 
 let producto6={
-    nombre:"producto6",
+    nombre:"blanqueador",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-macheo.appspot.com/o/blanqueador.png?alt=media&token=aab6855b-e4ba-43b4-97c9-08e2fc1f98d6"
 }
 
 let producto7={
-    nombre:"producto7",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"bolsa de basura",
+    precio:1000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-macheo.appspot.com/o/boolsa%20de%20basura.png?alt=media&token=9517da7c-1d12-46c9-b31e-10449a7f0bbc"
 }
 
 let producto8={
-    nombre:"producto8",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"gaseosa cola negra",
+    precio:2500,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-macheo.appspot.com/o/gaseosa%20cola%20negra.png?alt=media&token=dfce1e3a-9891-4a4c-9911-c3f3f2e535a7"
 }
 
 let producto9={
-    nombre:"producto9",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"gaseosa manzana",
+    precio:2500,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-macheo.appspot.com/o/gaseosa%20manzana.png?alt=media&token=a3a643d6-b163-4d19-b3f6-b3201d312697"
 }
 
 let producto10={
-    nombre:"producto10",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"javon lava platos",
+    precio:5000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-macheo.appspot.com/o/javon%20lava%20platas.png?alt=media&token=d83f7ae8-a8be-42e6-a858-b83287d90290"
 }
 
 let producto11={
-    nombre:"producto11",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"leche en polvo",
+    precio:4000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-macheo.appspot.com/o/leche%20en%20polvo.png?alt=media&token=61b6b6ac-94b2-479e-a5b5-f5b8ba14dd44"
 }
 
 let producto12={
-    nombre:"producto12",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"leche",
+    precio:2500,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-macheo.appspot.com/o/leche.png?alt=media&token=588cd241-6d6b-47eb-af68-153fa117cce7"
 }
 
 let producto13={
     nombre:"producto13",
     precio:500000,
-    estado:"Nuevo",
+    estado:true,
     foto:""
 }
 
 let producto14={
-    nombre:"producto14",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"mantequilla",
+    precio:4500,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-macheo.appspot.com/o/mantequilla.png?alt=media&token=62495c47-13ba-48ab-8910-218a3192992e"
 }
 
 let producto15={
-    nombre:"producto15",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"manzana verde",
+    precio:1300,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-macheo.appspot.com/o/manzana.png?alt=media&token=8307cf7e-69a2-49c8-a4d5-28d3f0a2e05c"
+}
+
+let producto16={
+    nombre:"palito de queso",
+    precio:3000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-macheo.appspot.com/o/palito%20de%20queso.png?alt=media&token=51ab723b-8178-48f0-8aec-731f2c5333e4"
+}
+
+let producto17={
+    nombre:"portatil",
+    precio:300000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-macheo.appspot.com/o/portatil.jpg?alt=media&token=5198be93-890e-45f1-b610-5f37bb677b7f"
+}
+
+let producto18={
+    nombre:"roll on mujer",
+    precio:4000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-macheo.appspot.com/o/roll%20on%20muerj.png?alt=media&token=4c7c0dfd-9957-401b-9d5d-5b16bee19c8d"
+}
+
+let producto19={
+    nombre:"roll on hombre",
+    precio:4000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-macheo.appspot.com/o/rool%20on.png?alt=media&token=cddeea41-4125-4080-89bd-db561307b1f4"
+}
+
+let producto20={
+    nombre:"shampoo anticaspa",
+    precio:5000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tienda-macheo.appspot.com/o/shampoo%20anticaspa.png?alt=media&token=36e80b19-5978-4f3e-b69a-1b627dfbbf6c"
 }
 
 //ARREGLO
@@ -121,6 +156,12 @@ let productos=Array(
     producto13,
     producto14,
     producto15
+    producto16
+    producto17
+    producto18
+    producto19
+    producto20
+
 )
 
 //RECORRO EL ARREGLO
